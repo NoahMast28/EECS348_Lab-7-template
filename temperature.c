89826c04af9b5b25e89a4a9028118c62bed991ea
Noah Mast <noahmast28@gmail.com>
2025-03-26 17:31:25 -0500

    Implemented Temperature Conversion Utility

--- temperature.c
+++ temperature.c
@@ -1,1 +1,45 @@
 #include "temperature.h"
+#include <stdio.h>
+
+float celsius_to_fahrenheit(float celsius) {
+    return (celsius * (9.0 / 5.0)) + 32;
+}
+
+float fahrenheit_to_celsius(float fahrenheit) {
+    return (fahrenheit - 32) * (5.0 / 9.0);
+}
+
+float celsius_to_kelvin(float celsius) {
+    return celsius + 273.15;
+}
+
+float kelvin_to_celsius(float kelvin) {
+    if (kelvin < 0) {
+        printf("Error: Kelvin temperatures cannot be negative\n");
+        return -1
+    }
+
+    return kelvin - 273.15;
+}
+
+float fahrenheit_to_kelvin(float fahrenheit) {
+    return celsius_to_kelvin(fahrenheit_to_celsius(fahrenheit));
+}
+
+float kelvin_to_fahrenheit(float kelvin) {
+    return celsius_to_fahrenheit(kelvin_to_celsius(kelvin));
+}
+
+void categorize_temperature(float_celsius) {
+    if (celsius < 0) {
+        printf("Category: Freezing - Wear winter gear.\n");
+    } else if (celsius < 10) {
+        printf("Category: Cold - Wear a jacket.\n");
+    } else if (celsius < 25) {
+        printf("Category: Comfortable - Enjoy the nice weather!\n");
+    } else if (celsius < 35) {
+        printf("Category: Hot - Make sure to drink lots of water.\n");
+    } else {
+        printf("Category: Extreme Heat - Avoid being outside for too long.\n");
+    }
+}
--- temperature_main.c
+++ temperature_main.c
@@ -1,5 +1,67 @@
 #include "temperature.h"
 
-int main(int argc, char *argv[]) {
+int main() {
+    float temperature;
+    char input_scale, target_scale;
+
+    printf("Enter a temperature value: ");
+
+    if (scanf("%f", &temperature) != 1) {
+        printf("Invalid input. Please enter a floating point value.\n");
+        return 1;
+    }
+
+    printf("Enter the input scale: ");
+    scanf("%c", input_scale);
+
+    printf("Enter the target scale: ");
+    scanf("%c", target_scale);
+
+    float converted_temperature;
+
+    if (input_scale == 'C' || input_scale == 'c') {
+        if (target_scale == 'F' || target_scale == 'f') {
+            converted_temperature = celsius_to_fahrenheit(temperature);
+        } else if (target_scale == 'K' || target_scale == 'k') {
+            converted_temperature = celsius_to_kelvin(temperature);
+        } else {
+            printf("Invalid target temperature scale.\n");
+            return 1;
+        }
+    } else if (input_scale == 'F' || input_scale == 'f') {
+        if (target_scale == 'C' || target_scale == 'c') {
+            converted_temperature = fahrenheit_to_celsius(temperature);
+        } else if (target_scale == 'K' || target_scale == 'k') {
+            converted_temperature = fahrenheit_to_kelvin(temperature);
+        } else {
+            printf("Invalid target temperature scale.\n");
+            return 1;
+        }
+    } else if (input_scale == 'K' || input_scale == 'k') {
+        if (target_scale == 'F' || target_scale == 'f') {
+            converted_temperature = kelvin_to_fahrenheit(temperature);
+        } else if (target_scale == 'C' || target_scale == 'c') {
+            converted_temperature = kelvin_to_celsius(temperature);
+        } else {
+            printf("Invalid target temperature scale.\n");
+            return 1;
+        }
+    } else {
+        printf("Invalid input scale.\n");
+        return 1;
+    }
+
+    float celsius_temp;
+    if (input_scale == 'F' || input_scale == 'f') {
+        celsius_temp = fahrenheit_to_celsius(temperature);
+    } else if (input_scale == 'K' || input_scale == 'k') {
+        celsius_temp = kelvin_to_celsius(temperature);
+    } else {
+        celsius_temp = temperature;
+    }
+
+    printf("Converted Temperature: %.2f %c\n", converted_temperature, target_scale);
+    categorize_temperature(celsius_temp);
+
     return 0;
 }
