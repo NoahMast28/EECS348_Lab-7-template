9f1c1c2170f02144ec91a1849951e56037468cac
Noah Mast <noahmast28@gmail.com>
2025-03-26 20:08:48 -0500

    Implemented Football Score Combination Finder

--- football.c
+++ football.c
@@ -1,1 +1,43 @@
 #include "football.h"
+#include <stdio.h>
+
+// Counts the number of combinations for a given point value
+int count_combinations(int points) {
+    int count = 0;
+
+    // The code for this looks a bit absurd, however the logic isn't that complicated
+    // Brute force checks each possible combination and increments count if it adds up to the right number of points
+    for (int td_2 = 0; td_2 * 8 <= points; td_2++) {
+        for (int td_fg = 0; td_fg * 7 <= points; td_fg++) {
+            for (int td = 0; td * 6 <= points; td++) {
+                for (int fg = 0; fg * 3 <= points; fg++) {
+                    for (int safety = 0; safety * 2 <= points; safety++) {
+                        if ((td_2 * 8) + (td_fg * 7) + (td * 6) + (fg * 3) + (safety * 2) == points) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return count;
+}
+
+// Prints each possible combination of scoring plays for a given point value
+void print_combinations(int points) {
+    // Brute force checks each possible combination and prints if the plays add up to the right number of points
+    for (int td_2 = 0; td_2 * 8 <= points; td_2++) {
+        for (int td_fg = 0; td_fg * 7 <= points; td_fg++) {
+            for (int td = 0; td * 6 <= points; td++) {
+                for (int fg = 0; fg * 3 <= points; fg++) {
+                    for (int safety = 0; safety * 2 <= points; safety++) {
+                        if ((td_2 * 8) + (td_fg * 7) + (td * 6) + (fg * 3) + (safety * 2) == points) {
+                            printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety\n", td_2, td_fg, td, fg, safety);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- football_main.c
+++ football_main.c
@@ -1,5 +1,29 @@
 #include "football.h"
+#include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main() {
+    int points;
+
+    // Executes until the user enters a 0 or 1 to exit
+    while (1) {
+        printf("Enter 0 or 1 to STOP\n");
+        printf("Enter the NFL score: ");
+
+        // Prints an error if the user does not input an integer or if their integer is negative
+        if (scanf("%d", &points) != 1 || points < 0) {
+            printf("Invalid input. Enter a nonnegative integer.\n");
+            return 1;
+        }
+
+        // Exits the loop if the user entered 0 or 1
+        if (points == 0 || points == 1) {
+            break;
+        }
+
+        // Prints the total number of combinations and the combinations
+        printf("Total combinations: %d\n", count_combinations(combinations));
+        print_combinations(points);
+    }
+    
     return 0;
 }
